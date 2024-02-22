2c629fbad355b5925aadb6b1f47c5987e1d175c4
Beatriz Cruz <mllebia@live.com>
2024-02-22 13:31:29 -0300

    some begin files

---
+++ listPlay.py
@@ -0,0 +1,14 @@
+
+
+phrase = "you have to be your greatest for yourself first"
+letter = 'y'
+count = 0
+
+
+for letter in phrase:
+        if letter == 'y':
+            count = count + 1
+        else:
+            pass
+
+print(count)
