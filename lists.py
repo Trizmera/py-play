ff43b86e8be40a94cc6bbaacb2b1595042c5d540
Beatriz Cruz <mllebia@live.com>
2024-02-26 20:47:29 -0300

    strings

---
+++ lists.py
@@ -0,0 +1,4 @@
+listOne = [1,2,3]
+listTwo = [4,5,6]
+
+print(listOne + listTwo)
---
+++ stringPlay.py
@@ -0,0 +1,11 @@
+
+str = 'BUCKFIELD'
+
+print(str[-1])
+print(str[-3])
+print(str[-2:])
+print(str[-4:-1])
+print(str[-7:-2])
+
+print(str[::-1])
+print(str[-9])
